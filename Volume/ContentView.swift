d774fe28acd6daccedb9a556e5868837bd0fb831
Yana Sang <yanawsang@gmail.com>
2020-10-06 17:09:38 -0400

    Initialize project and add gitignore

---
+++ Volume/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Volume
+//
+//  Created by Yana Sang on 10/6/20.
+//  Copyright © 2020 Cornell AppDev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
