c0425511c2fcec18ef23779f3a3c1962e872ca96
Cameron Russell <camrussell215@gmail.com>
2020-10-28 16:51:51 -0400

    Icons and Basic setup

---
+++ Volume/Extensions/Color+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  Color+ColorScheme.swift
+//  Volume
+//
+//  Created by Cameron Russell on 10/28/20.
+//  Copyright © 2020 Cornell AppDev. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    
+    public static let _lightGray = Color(white: 153/255, opacity: 1)
+    
+    public static let _orange = Color(red: 208/255, green: 112/255, blue: 0/255, opacity: 1)
+    
+}
