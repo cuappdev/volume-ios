8df1141cbc79a0353b90f0f60f3beaf622d371ce
= <daniel.vebman@gmail.com>
2020-11-07 21:53:21 -0500

    Create UnderlinedView

--- Volume/Views/UnderlinedText.swift
+++ Volume/Views/UnderlinedText.swift
@@ -10,20 +10,54 @@ import SwiftUI
 
 struct UnderlinedText: View {
     @State var text: String
+    @State private var textSize: CGSize = .zero
 
     var body: some View {
         VStack {
             Text(text)
+                .fixedSize()
+                .background(SizeGetter(size: $textSize))
+
             Image("underline")
-                .scaledToFill()
-                .frame(height: 12)
+                .resizable()
+                .scaledToFit()
+                // 112x6
+                .frame(width: textSize.width + 4, height:
+                    (textSize.width + 4) * 6 / 112)
+                .clipped()
+                .padding(.top, -12 * textSize.height / 20)
+                .offset(x: 2)
         }
     }
 }
 
 struct UnderlinedText_Previews: PreviewProvider {
     static var previews: some View {
-        UnderlinedText(text: "The Big Read")
-            .font(.custom("Futura-Bold", size: 15))
+        PreviewWrapper()
+    }
+
+    struct PreviewWrapper: View {
+        var body: some View {
+            UnderlinedText(text: "THE BIG READ AND OTHER THINGS")
+                .font(.custom("Futura-Bold", size: 15))
+        }
+    }
+}
+
+struct SizeGetter: View {
+    @Binding var size: CGSize
+
+    var body: some View {
+        return GeometryReader { geometry in
+            self.makeView(geometry: geometry)
+        }
+    }
+
+    func makeView(geometry: GeometryProxy) -> some View {
+        DispatchQueue.main.async {
+            self.size = geometry.frame(in: .global).size
+        }
+
+        return Rectangle().fill(Color.clear)
     }
 }
