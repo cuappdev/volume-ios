3b26d1bdaebdb1be64298c998d353be7426f15dc
Hanzheng Li <han@Hanzhengs-MacBook-Pro-2.local>
2021-11-29 22:42:02 -0500

    Update schema, add query and models

---
+++ Volume/Models/WeeklyDebrief.swift
@@ -0,0 +1,46 @@
+//
+//  WeeklyDebrief.swift
+//  Volume
+//
+//  Created by Hanzheng Li on 11/17/21.
+//  Copyright © 2021 Cornell AppDev. All rights reserved.
+//
+
+import Apollo
+import Foundation
+
+struct WeeklyDebrief {
+    /* createdAt
+     expirationDate
+     numShoutouts
+     readArticles {
+       ...articleFields
+     }
+     randomArticles {
+       ...articleFields
+     }
+     */
+    var creationDate: Date
+    var expirationDate: Date
+    var numShoutouts: Int
+    var numReadArticles: Int
+    var numBookmarkedArticles: Int
+    var readArticles: [Article]
+    var randomArticles: [Article]
+    
+    init(from weeklyDebrief: WeeklyDebriefFields) {
+        /*
+         schema changes required:
+         - createdAt -> creationDate
+         - numShoutouts, numReadArticles, numBookmarkedArticles -> type Int
+         */
+        creationDate = Date.from(iso8601: weeklyDebrief.createdAt)
+        expirationDate = Date.from(iso8601: weeklyDebrief.expirationDate)
+        numShoutouts = Int(weeklyDebrief.numShoutouts)
+        numReadArticles = Int(weeklyDebrief.numReadArticles)
+        numBookmarkedArticles = Int(weeklyDebrief.numBookmarkedArticles)
+        readArticles = [Article](weeklyDebrief.readArticles.map(\.fragments.articleFields))
+        randomArticles = [Article](weeklyDebrief.randomArticles.map(\.fragments.articleFields))
+    }
+    
+}
