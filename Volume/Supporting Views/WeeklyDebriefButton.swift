9e033d5df1fedbe4266fa16baa096556c53ac1f8
dishanest <hl999@cornell.edu>
2022-10-29 18:08:11 -0400

    Add deeplinking and weekly debrief

---
+++ Volume/Supporting Views/WeeklyDebriefButton.swift
@@ -0,0 +1,21 @@
+//
+//  WeeklyDebriefButton.swift
+//  Volume
+//
+//  Created by Hanzheng Li on 10/29/22.
+//  Copyright © 2022 Cornell AppDev. All rights reserved.
+//
+
+import SwiftUI
+
+struct WeeklyDebriefButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeeklyDebriefButton_Previews: PreviewProvider {
+    static var previews: some View {
+        WeeklyDebriefButton()
+    }
+}
